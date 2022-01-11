ffb5d822114a4398dbd4b302542e9dde515ec012
CaptainJang316 <gksrkfkaryghl@gmail.com>
2022-01-12 00:23:45 +0900

    apply css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,30 +1,33 @@
 import ProtoTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css"
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
     return (
-        <div>
-          <img src={coverImg} />
-          <h2>
-            <Link to={`/movie/${id}`}>{title}</Link>
-          </h2>
-          <p>{summary}</p>
-          {/* {(movie.hasOwnProperty("genres")?
-          <ul>
+        <div className={styles.item}>
+          <div><img src={coverImg} className={styles.img}/></div>
+          <div className={styles.text}>
+            <h2>
+              <Link to={`/movie/${id}`} className={styles.title}>{title}</Link>
+            </h2>
+            {year}, 
             {genres.map((g) => (
-              <li key={g}>{g}</li>
-            ))}
-          </ul> : null)} */}
-          <ul>
-            {genres&&genres.map((g) => (
-              <li key={g}>{g}</li>
-            ))}
-          </ul>
+                ` ${g} |`
+              ))} 
+            <p>{summary.length > 335 ? `${summary.slice(0, 235)}...` : summary}</p>
+            {/* {(movie.hasOwnProperty("genres")?
+            <ul>
+              {genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul> : null)} */}
+          </div>
         </div>
     );
 }
 
 Movie.protoTypes = {
+    year:  ProtoTypes.number.isRequired,
     id: ProtoTypes.number.isRequired, 
     coverImg: ProtoTypes.string.isRequired,
     title: ProtoTypes.string.isRequired,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie"
+import styles from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -17,15 +18,16 @@ function Home() {
     getMovies();
   }, []);
 
-  return <div> 
+  return <div className={styles.container}> 
    {loading ? (
-    <h1>Loading...</h1>
+    <h1 className={styles.loading}>Loading...</h1>
   ) : (
-    <div>
+    <div className={styles.movies}>
       {movies.map((movie) => (
         <Movie
           key={movie.id}
           id={movie.id}
+          year={movie.year}
           coverImg={movie.medium_cover_image}
           title={movie.title}
           summary={movie.summary}
