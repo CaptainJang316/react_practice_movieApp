d50c6affafab3aeb0d8f35fbcca06e417fb9c5e4
CaptainJang316 <gksrkfkaryghl@gmail.com>
2022-01-13 17:38:45 +0900

    movie-detail dialog(not finished yet)

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,50 @@
 import ProtoTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styles from "./Movie.module.css"
+import Detail from "../routes/Detail"
+
+import * as React from 'react';
+
+
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered:  { opacity: 1 },
+  exiting:  { opacity: 0 },
+  exited:  { opacity: 0 },
+};
+
 
 function Movie({ id, coverImg, title, year, summary, genres }) {
     return (
         <div className={styles.item}>
           <div><img src={coverImg} className={styles.img}/></div>
           <div className={styles.text}>
-            <h2>
-              <Link to={`/movie/${id}`} className={styles.title}>{title}</Link>
-            </h2>
+          <Detail
+            id={id}
+            year={year}
+            coverImg={coverImg}
+            title={title}
+            summary={summary}
+            genres={genres}
+           />
+            {/* <h2 onClick={Detail.handleClickOpen()}>
+              <Detail movie={movie}/>
+              <Link to={{
+                pathname: `/movie/${id}`,
+                state: {
+                  movie: movie
+                },
+              }} className={styles.title}>{title}</Link>
+            </h2> */}
             {year}, 
             {genres.map((g) => (
                 ` ${g} |`
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,89 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-function Detail() {
-    const {id} = useParams();
-    const getMovie = async () => {
-        const json = await (
-            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-        ).json();
-        console.log(json);
-    };
+import styles from "./Detail.module.css"
 
-    useEffect(() => {
-        getMovie();
-    }, []);
+export default function Detail({id, coverImg, title, year, summary, genres}) {
+  const [open, setOpen] = React.useState(false);
+  //const [scroll, setScroll] = React.useState('paper');
 
-    return <h1>Detail</h1>;
+  const handleClickOpen = () => () => {
+    setOpen(true);
+    //setScroll();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <h2 onClick={handleClickOpen()}>{title}</h2>
+      {/* <Button onClick={handleClickOpen('paper')}>scroll=paper</Button>
+      <Button onClick={handleClickOpen('body')}>scroll=body</Button> */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        //scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">{title}</DialogTitle>
+        <DialogContent dividers={true}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+          <div className={styles.item}>
+          <div><img src={coverImg} className={styles.img}/></div>
+          <div className={styles.text}>
+            <h2>
+            {title}
+            </h2>
+            {year}, 
+            {genres.map((g) => (
+                ` ${g} |`
+              ))} 
+            <p>{summary}</p>
+            {/* {(movie.hasOwnProperty("genres")?
+            <ul>
+              {genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul> : null)} */}
+          </div>
+        </div>
+            {/* {[...new Array(50)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+              )
+              .join('\n')} */}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
-export default Detail;
